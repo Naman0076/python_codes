e0ea9e5901e4f132afeeeb7de7f28441852f5c6e
Naman Kaushik <naman.kaushik@impactqa.com>
2025-04-07 15:14:53 +0530

    first commit

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -2,22 +2,71 @@
 squares = {x: x**2 for x in range(1, 6)}
 print(squares)
 
+squares = {}
+
+for x in range(1, 6):
+    squares[x] = x ** 2
+
+print(squares)
+
+
 # Filtering 
 even_squares = {x: x**2 for x in range(1, 11) if x % 2 == 0}
+print(even_squares)
+
+
+even_squares = {}
+
+for x in range(1, 11):
+    if x % 2 == 0:
+        even_squares[x] = x ** 2
+
 print(even_squares)
 
 # Dictionary Comphrension
 labels = {x: "Even" if x % 2 == 0 else "Odd" for x in range(1, 6)}
 print(labels)
 
+
+labels = {}
+
+for x in range(1, 6):
+    if x % 2 == 0:
+        labels[x] = "Even"
+    else:
+        labels[x] = "Odd"
+
+print(labels)
+
+
 # Swapping key and values 
 original = {"a": 1, "b": 2, "c": 3}
 reversed_dict = {v: k for k, v in original.items()}
 print(reversed_dict)
 
+
+original = {"a": 1, "b": 2, "c": 3}
+reversed_dict = {}
+
+for k, v in original.items():
+    reversed_dict[v] = k
+
+print(reversed_dict)
+
+
 # Filtering words by length
 words = ["apple", "bat", "cherry", "dog", "elephant"]
 long_words = {word: len(word) for word in words if len(word) > 4}
+print(long_words)
+
+
+words = ["apple", "bat", "cherry", "dog", "elephant"]
+long_words = {}
+
+for word in words:
+    if len(word) > 4:
+        long_words[word] = len(word)
+
 print(long_words)
 
 # Create a dictionary from two lists 
@@ -26,9 +75,28 @@ values = ["Alice", 25, "New York"]
 person = {k: v for k, v in zip(keys, values)}
 print(person)
 
+keys = ["name", "age", "city"]
+values = ["Alice", 25, "New York"]
+
+person = {}
+
+for k, v in zip(keys, values):
+    person[k] = v
+
+print(person)
+
+
 # Counting chracter occurences
 text = "hello world"
 char_count = {char: text.count(char) for char in set(text)}
+print(char_count)
+
+text = "hello eorld"
+char_count = {}
+
+for char in set(text):
+    char_count[char] = text.count(char)
+
 print(char_count)
 
 # Filtering dictionary 
@@ -36,14 +104,50 @@ scores = {"Alice": 45, "Bob": 82, "Charlie": 60, "David": 30}
 high_scores = {k: v for k, v in scores.items() if v > 50}
 print(high_scores)
 
+
+scores = {"Alice": 45, "Bob": 82, "Charlie": 60, "David": 30}
+high_scores = {}
+
+for k, v in scores.items():
+    if v > 50:
+        high_scores[k] = v
+print(high_scores)
+
+
 # Generate multiplication table 
 table_5 = {x: x * 5 for x in range(1, 11)}
 print(table_5)
+
+
+table_5 = {}
+
+for x in range(1, 11):
+    table_5[x] = x * 5
+
+print(table_5)
+
 
 # Nested dictionary comphrension
 numbers = {x: {"square": x**2, "cube": x**3} for x in range(1, 6)}
 print(numbers)
 
+
+numbers = {}
+
+for x in range(1, 6):
+    numbers[x] = {
+        "square": x ** 2,
+        "cube": x ** 3
+    }
+
+print(numbers)
+
+
 # Generate a dict
 squares_dict = {x: x**2 for x in range(1, 6)}
 print(squares_dict)
+
+x = {}
+for i in range(1,6):
+    x[i] = i**2
+print(x)
--- list_comprehensions.py
+++ list_comprehensions.py
@@ -2,28 +2,61 @@
 squares = [x**2 for x in range(1, 11)]
 print(squares)
 
+x = []
+for i in range(1, 11):
+    x.append(i**2)
+
 # Basic with filtering / conditions
 evens = [x for x in range(1, 21) if x % 2 == 0]
 print(evens)
 
+x = []
+for i in range (1, 21):
+    if i %2 == 0:
+        x.append(i)
+
 # NEsted Loops
 coordinates = [(x, y) for x in [1, 2] for y in [3, 4]]
 print(coordinates)
+
+a = []
+for x in [1, 2]:
+    for y in [3, 4]:
+        a.append((x, y))
+print(a)
 
 # flatten a 2D list (Matrix)
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 flat_list = [num for row in matrix for num in row]
 print(flat_list)
 
+n = len(matrix)
+m = len(matrix[0])
+a = []
+for i in range(n):
+    for j in range(m):
+        a.append(matrix[i][j])
+
 # with functions
 words = ["hello", "world", "python"]
 uppercase_words = [word.upper() for word in words]
 print(uppercase_words)
 
+x = ["hello", "world", "python"]
+for i in range(len(x)):
+    x[i] = x[i].upper()
+print(x)
+
 # Remove vowels from a string
 text = "List comprehensions are awesome!"
 consonants = "".join([ch for ch in text if ch.lower() not in "aeiou"])
 print(consonants)
+
+c = ""
+for ch in text:
+    if ch.lower() not in "aeiou":
+        c += ch
+print(c)
 
 # Common element in two lists
 list1 = [1, 2, 3, 4, 5]
@@ -31,6 +64,25 @@ list2 = [3, 4, 5, 6, 7]
 common_elements = [x for x in list1 if x in list2]
 print(common_elements)
 
+c_ele = []
+for x in list1:
+    if x in list2:
+        c_ele.append(x)
+
+
 # List of prime numbers
 primes = [x for x in range(2, 31) if all(x % d != 0 for d in range(2, int(x**0.5) + 1))]
+print(primes)
+
+primes = []
+
+for x in range(2, 31):
+    is_prime = True
+    for d in range(2, int(x ** 0.5) + 1):
+        if x % d == 0:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(x)
+
 print(primes)
