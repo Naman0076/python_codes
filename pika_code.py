47e1f6604fa4454166effe61fb0041d4435f8bf3
Naman Kaushik <naman.kaushik@impactqa.com>
2025-05-05 10:38:05 +0530

    fifth commit

--- pika_code.py
+++ pika_code.py
@@ -1,43 +1,86 @@
+#publisher.py
+import pika
+
+class Publisher:
+    def __init__(self, config):
+        self.config = config
+
+    def publish(self, routing_key, message):       
+        connection = self.create_connection()
+        # Create a new channel
+        channel = connection.channel()
+        # Creates an exchange if it does not already exist
+        channel.exchange_declare(exchange=self.config['exchange'],
+                                 exchange_type='topic')
+        # Publishes message to the exchange with the given routing key
+        channel.basic_publish(exchange=self.config['exchange'],
+                              routing_key=routing_key, body=message)
+        print(" [x] Sent message %r for %r" % (message, routing_key))
+
+    # Create new connection
+    def create_connection(self):
+        param = pika.ConnectionParameters(
+            host=self.config['host'],
+            port=self.config['port']
+        )
+        return pika.BlockingConnection(param)
+
+config = {'host': 'localhost', 'port': 5672, 'exchange': 'my_exchange'}
+publisher = Publisher(config)
+publisher.publish('nse.nifty', 'New Data')
+
+
+#subscriber.py
 import pika
 import sys
- 
-def publish_message(routing_key, message, config):
-    # Establish connection and channel
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host=config['host'],
-        port=config['port']
-    ))
-    channel = connection.channel()
- 
-    # Declare the exchange
-    channel.exchange_declare(
-        exchange=config['exchange'],
-        exchange_type='topic'
-    )
- 
-    # Publish message to exchange with routing key
-    channel.basic_publish(
-        exchange=config['exchange'],
-        routing_key=routing_key,
-        body=message
-    )
- 
-    print(f" [x] Sent '{message}' with routing key '{routing_key}'")
- 
-    connection.close()
- 
-# === MAIN EXECUTION ===
-config = {
-    'host': 'localhost',
-    'port': 5672,
-    'exchange': 'my_exchange'
-}
- 
+
+class Subscriber:
+    def __init__(self, queueName, bindingKey, config):
+        self.queueName = queueName
+        self.bindingKey = bindingKey
+        self.config = config
+        self.connection = self._create_connection()
+
+    def __del__(self):
+        self.connection.close()
+
+    def _create_connection(self):
+        parameters = pika.ConnectionParameters(
+            host=self.config['host'],
+            port=self.config['port']
+        )
+        return pika.BlockingConnection(parameters)
+
+    def on_message_callback(self, channel, method, properties, body):
+        binding_key = method.routing_key
+        print("received new message for - " + binding_key)
+
+    def setup(self):
+        channel = self.connection.channel()
+        channel.exchange_declare(exchange=self.config['exchange'],
+                                 exchange_type='topic')
+        # This method creates or checks a queue
+        channel.queue_declare(queue=self.queueName)
+        # Binds the queue to the specified exchange
+        channel.queue_bind(queue=self.queueName,
+                           exchange=self.config['exchange'],
+                           routing_key=self.bindingKey)
+        channel.basic_consume(queue=self.queueName,
+                              on_message_callback=self.on_message_callback,
+                              auto_ack=True)
+        print(' [*] Waiting for data for ' + self.queueName + '. To exit press CTRL+C')
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
+
+config = {'host': 'localhost', 'port': 5672, 'exchange': 'my_exchange'}
+
 if len(sys.argv) < 3:
-    print(f"Usage: python {sys.argv[0]} <RoutingKey> <Message>")
-    sys.exit(1)
- 
-routing_key = sys.argv[1]
-message = ' '.join(sys.argv[2:])
- 
-publish_message(routing_key, message, config)
+    print('Usage: ' + __file__ + ' <QueueName> <BindingKey>')
+    sys.exit()
+else:
+    queueName = sys.argv[1]
+    key = sys.argv[2]  # key in the form exchange.*
+    subscriber = Subscriber(queueName, key, config)
+    subscriber.setup()
