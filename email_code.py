4e8b1d79b716eba061e788eb7346b3e1e6dc3a4f
Naman Kaushik <naman.kaushik@impactqa.com>
2025-06-03 14:46:35 +0530

    email commit

---
+++ email_code.py
@@ -0,0 +1,36 @@
+import smtplib
+import os 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+def send_email(recipient_email, subject, body):
+    try:
+        msg = MIMEMultipart()
+        msg["From"] = EMAIL_ADDRESS
+        msg["To"] = recipient_email
+        msg["Subject"] = subject
+
+        msg.attach(MIMEText(body, 'plain'))
+
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            server.sendmail(EMAIL_ADDRESS, recipient_email, msg.as_string())
+
+        print("Email Sent Successfully!!!")
+    except Exception as e:
+        print(f"error: {e}")
+
+if __name__ == "__main__":
+    recipient = "nikita.jain@impactqa.com"
+    subject = "!!Code Chal Gaya hehe!!"
+    body = "This proves ke code chal gaya using python and Gmail SMTP "
+    send_email(recipient, subject, body)
