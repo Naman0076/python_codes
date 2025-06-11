0cf6e01056f8546f31a30faf8fe0f2899a871deb
Naman Kaushik <naman.kaushik@impactqa.com>
2025-06-11 11:35:52 +0530

    logging commit

--- csv_filehandeling.py
+++ csv_filehandeling.py
@@ -1,7 +1,11 @@
 # Writing data to a CSV file
 import csv
+import logging
+import configure_logging
+ 
 with open("employees.csv", "w", newline="") as file:
     writer = csv.writer(file)
+    logging.info("Opening and writing a CSV file")
     
     writer.writerow(["Emp_ID", "Name", "Department", "Salary"])
     
@@ -11,23 +15,24 @@ with open("employees.csv", "w", newline="") as file:
         [103, "Charlie", "Finance", 55000]
     ])
 
-print("CSV file written successfully!")
+logging.info("CSV File written Successfully")
 
 
 #Reading a CSV file:
 with open("employees.csv", "r") as file:
     reader = csv.reader(file)
+    logging.info("opening CSV file in Read mode")
     
     for row in reader:
         print(row)
-
+logging.info(f"Prinitng row: {row}")
 
 #Appending new data to a CSV file
 with open("employees.csv", "a", newline="") as file:
     writer = csv.writer(file)
-    
+    logging.info(f"Opening and Appending in the CSV file {file}")
     # Adding new rows
     writer.writerow([104, "David", "Marketing", 52000])
     writer.writerow([105, "Eva", "Sales", 48000])
 
-print("Data appended to CSV successfully!")
+logging.info(f"Data appended successfully in the CSV fiel {file}")
